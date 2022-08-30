4dc971668c23afd206121b273bd93bca9edba37f
FedericoL <floaiza@unal.edu.co>
2022-08-30 15:16:24 -0500

    Se crean las clases Empresa, Empleado, MovimientoDinero

---
+++ src/main/java/com/example/sistema/Entities/MovimientoDinero.java
@@ -0,0 +1,37 @@
+package com.example.sistema.Entities;
+
+public class MovimientoDinero {
+    private float monto;
+    private String conceptoMovimiento;
+    private String nombreQuienModifico;
+
+    public MovimientoDinero(float monto, String conceptoMovimiento, String nombreQuienModifico) {
+        this.monto = monto;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.nombreQuienModifico = nombreQuienModifico;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getNombreQuienModifico() {
+        return nombreQuienModifico;
+    }
+
+    public void setNombreQuienModifico(String nombreQuienModifico) {
+        this.nombreQuienModifico = nombreQuienModifico;
+    }
+}
